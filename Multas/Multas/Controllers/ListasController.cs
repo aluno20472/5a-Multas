8a577fa0c2cc53a72eee1aec557d7421b11d2ca3
aluno20472 <aluno20472@gmail.com>
2019-05-30 14:32:53 +0100

    fesger

---
+++ Multas/Multas/Controllers/ListasController.cs
@@ -0,0 +1,42 @@
+﻿using Multas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Multas.Controllers
+{
+    public class ListasController : Controller
+    {
+
+        private MultasDB db = new MultasDB();
+
+        // GET: Listas
+        // [Authorize]
+        //   [Authorize(Roles = "Admin,Cliente")]
+        //[Authorize(Roles = "Cliente")]
+        //[Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+
+
+            Agentes agente = db.Agentes.Find(1);
+
+            Condutores condutor = db.Condutores.Find(1);
+
+            Multas.Models.Multas multa = db.Multas.Find(1);
+
+
+            var lista = new ListaViewModel();
+            lista.Agente = agente;
+            lista.Condutor = condutor;
+            lista.Multa = multa;
+
+
+            return View(lista);
+        }
+
+
+    }
+}
---
+++ Multas/Multas/Models/ListaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class ListaViewModel
+    {
+
+        public Condutores Condutor { get; set; }
+        public Multas Multa { get; set; }
+        public Agentes Agente { get; set; }
+    }
+}
